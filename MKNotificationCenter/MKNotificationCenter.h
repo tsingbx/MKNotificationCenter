d5c4eb0fd30f3d883b35991159ec662e20513343
xulingjiao <379559296@qq.com>
2018-10-30 12:55:23 +0800

    add all

---
+++ MKNotificationCenter/MKNotificationCenter.h
@@ -0,0 +1,20 @@
+//
+//  MKNotificationCenter.h
+//  MKNotificationCenter
+//
+//  Created by xulingjiao on 2018/10/30.
+//  Copyright © 2018年 xulingjiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MKNotificationCenter.
+FOUNDATION_EXPORT double MKNotificationCenterVersionNumber;
+
+//! Project version string for MKNotificationCenter.
+FOUNDATION_EXPORT const unsigned char MKNotificationCenterVersionString[];
+
+#import <MKNotificationCenter/KBNotificationCenterObserverProtocol.h>
+#import <MKNotificationCenter/KBNotificationCenterObserver.h>
+
+
